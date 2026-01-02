eb4922f059f45caf004791b4c763b2f1907a212f
pero4l <lilpero03@gmail.com>
2026-01-02 22:59:51 +0100

    feat: build the server

---
+++ zent_backend/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+require('dotenv').config();
+
+const app = express(); 
+
+app.get("/", (req, res) =>{
+    res.status(200).json({
+        "message": "Welcome to Zent",
+        "sucess": true
+    })
+})
+
+const PORT = process.env.PORT
+
+app.listen(PORT, () =>{
+    console.log(`✅ Database connected successfully and Server running on PORT:${PORT}`);
+    
+})
